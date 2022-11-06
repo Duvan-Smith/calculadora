8b6e8c057c805d3b3dd2ce1b13ea2f22663b5079
Duvan Roque <dsmith.mr@gmail.com>
2022-11-06 10:47:30 -0500

    Fin  interfas grafica

--- src/components/BotonCalc.tsx
+++ src/components/BotonCalc.tsx
@@ -1,19 +1,29 @@
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {styles} from '../theme/appTheme';
 
 interface Props {
   texto: string;
   color?: string;
+  ancho?: boolean;
 }
 
-export const BotonCalc = ({texto, color = '#2D2D2D'}: Props) => {
+export const BotonCalc = ({texto, color = '#2D2D2D', ancho = false}: Props) => {
   return (
-    <View
-      style={{
-        ...styles.boton,
-        backgroundColor: color,
-      }}>
-      <Text style={styles.botonTexto}>{texto}</Text>
-    </View>
+    <TouchableOpacity>
+      <View
+        style={{
+          ...styles.boton,
+          backgroundColor: color,
+          width: ancho ? 150 : 70,
+        }}>
+        <Text
+          style={{
+            ...styles.botonTexto,
+            color: color === '#2D2D2D' ? 'white' : 'black',
+          }}>
+          {texto}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
