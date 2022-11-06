e144e43691b6e272eb44ff0b1bbf5465684e4d40
Duvan Roque <dsmith.mr@gmail.com>
2022-11-06 10:23:23 -0500

    Primeros pasos vista

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -4,9 +4,35 @@ export const styles = StyleSheet.create({
   fondo: {
     flex: 1,
     backgroundColor: 'black',
+    paddingHorizontal: 10,
   },
-  texto: {
+  calculadoraContainer: {
+    flex: 1,
+    paddingHorizontal: 10,
+    justifyContent: 'flex-end',
+  },
+  resultado: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  resultadoPequeno: {
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  boton: {
+    height: 80,
+    width: 80,
+    backgroundColor: '#9B9B9B',
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  botonTexto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: '400',
   },
 });
