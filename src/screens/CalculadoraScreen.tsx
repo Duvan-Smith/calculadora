16fdc580650d9b7934fb662cacb1d99e96d78831
Duvan Roque <dsmith.mr@gmail.com>
2022-11-07 08:43:31 -0500

    Validaciones teclado numerico

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -12,7 +12,30 @@ export const CalculadoraScreen = () => {
   };
 
   const armarNumero = (numeroTexto: string) => {
-    setNumero(numero + numeroTexto);
+    if (numero.includes('.') && numeroTexto === '.') return;
+    if (numero.startsWith('0') || numero.startsWith('-0')) {
+      if (numeroTexto === '.') {
+        setNumero(numero + numeroTexto);
+      } else if (numeroTexto === '0' && numero.includes('.')) {
+        setNumero(numero + numeroTexto);
+      } else if (numeroTexto !== '0' && !numero.includes('.')) {
+        setNumero(numeroTexto);
+      } else if (numeroTexto === '0' && !numero.includes('.')) {
+        setNumero(numeroTexto);
+      } else {
+        setNumero(numero + numeroTexto);
+      }
+    } else {
+      setNumero(numero + numeroTexto);
+    }
+  };
+
+  const positivoNegativo = () => {
+    if (numero.includes('-')) {
+      setNumero(numero.replace('-', ''));
+    } else {
+      setNumero('-' + numero);
+    }
   };
 
   return (
@@ -25,7 +48,7 @@ export const CalculadoraScreen = () => {
 
       <View style={styles.fila}>
         <BotonCalc texto="c" color="#9B9B9B" accion={limpiar} />
-        <BotonCalc texto="+/-" color="#9B9B9B" accion={limpiar} />
+        <BotonCalc texto="+/-" color="#9B9B9B" accion={positivoNegativo} />
         <BotonCalc texto="del" color="#9B9B9B" accion={limpiar} />
         <BotonCalc texto="/" color="#FF9427" accion={limpiar} />
       </View>
